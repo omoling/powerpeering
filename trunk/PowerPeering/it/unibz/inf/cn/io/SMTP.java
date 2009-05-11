3032686656c5142f590778ed9cb4847772a12f58
anton.dignoes <anton.dignoes@0eea14b6-34b9-11de-a996-7ff70a941223>
2009-05-11 09:15:40 +0000

    removed useless method

--- trunk/PowerPeering/it/unibz/inf/cn/io/SMTP.java
+++ trunk/PowerPeering/it/unibz/inf/cn/io/SMTP.java
@@ -9,48 +9,6 @@ import javax.mail.*;
 import javax.mail.internet.*;
 
 public class SMTP {
-
-	public static void send2(String serverHost, String from, String to,
-			String subject, String bodyText, File attachment)
-			throws AddressException, MessagingException {
-
-		Properties props = System.getProperties();
-		props.put("mail.smtp.host", serverHost);
-		Session session = Session.getDefaultInstance(props, null);
-
-		// create a new msg
-		Message msg = new MimeMessage(session);
-
-		msg.setFrom(new InternetAddress(from));
-		msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to,
-				false));
-		msg.setSubject(subject);
-		msg.setHeader("X-Mailer", "PowerPeering");
-		msg.setSentDate(new Date());
-
-		Multipart multipart = new MimeMultipart();
-		MimeBodyPart messageBodyPart;
-		// create the text part
-		messageBodyPart = new MimeBodyPart();
-		if(bodyText != null) {
-			messageBodyPart.setText(bodyText);
-		}
-		multipart.addBodyPart(messageBodyPart);
-
-		// create the attachment part
-		messageBodyPart = new MimeBodyPart();
-		if(attachment != null) {
-			DataSource source = new FileDataSource(attachment);
-			messageBodyPart.setDataHandler(new DataHandler(source));
-			messageBodyPart.setFileName(attachment.getName());
-		}
-		multipart.addBodyPart(messageBodyPart);
-
-		// Put parts in message
-		msg.setContent(multipart);
-
-		Transport.send(msg);
-	}
 	
 	public static void send(String serverHost, String from, String to,
 			String subject, String bodyText)
